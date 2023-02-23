f740c645645515a8db0f280a5ae34953d48f419d
Soldy <4786022+Soldy@users.noreply.github.com>
2023-02-23 09:51:04 +0000

    global in test is fine nowhere else.. just in the test is fine
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,25 +1,36 @@
+"""
+" pytest
+"""
 import pytest
 import scrapper
 
 offers_list = []
 def test_offerget():
+    """ offer  """
     global offers_list
     with scrapper.ScrapperLayer() as scrap:
         offers_list = scrap.pageGet("https://wltest.dns-systems.net/")
     return True
 @pytest.mark.skipif(not test_offerget(), reason='scrapp failed')
 def test_offersize():
+    """ response size test  """
     global offers_list
     assert len(offers_list) == 6
+@pytest.mark.skipif(not test_offerget(), reason='scrapp failed')
 def test_nofailed():
+    """ searching failed in result  """
     global offers_list
     for i in range(6):
-        assert ('failed' in offers_list[i]) == False
+        assert ('failed' in offers_list[i]) is False
+@pytest.mark.skipif(not test_offerget(), reason='scrapp failed')
 def test_hasprice():
+    """ price exist check """
     global offers_list
     for i in range(6):
-        assert ('price' in offers_list[i]) == True
+        assert ('price' in offers_list[i]) is True
+@pytest.mark.skipif(not test_offerget(), reason='scrapp failed')
 def test_correctorder():
+    """ order check """
     global offers_list
     for i in range(5):
-        assert offers_list[i]['price'] > offers_list[i+1]['price'] 
+        assert offers_list[i]['price'] > offers_list[i+1]['price']
