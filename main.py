ed60d7341a22477dc8532d2d8478ef71e8e86705
Soldy <4786022+Soldy@users.noreply.github.com>
2023-02-23 09:46:04 +0000

    clean up
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+"""
+Simple scrapper main loop
+"""
 import json
 import scrapper
 
 with scrapper.ScrapperLayer() as scrap:
-    
-
     file = open('out.json', 'w')
     file.write(json.dumps(scrap.pageGet("https://wltest.dns-systems.net/")))
     file.close()
-
-
--- processor.py
+++ processor.py
@@ -1,15 +1,23 @@
+"""
+" Offer Processor
+"""
 import re
 import copy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def _priceRegExp(text:str, multi:int)->float:
+    """ Regular expectation helper function. DRY regarding."""
+    return float(multi*int(100*float(re.findall(r'\d+\.\d+', text)[0])))/100
+
 class OfferProcessor:
-    # Fail manager function for debugging purposes.
+    """ offer processor class """
     def _fail(self, name:str)->None:
+        """ Fail manager function for debugging purposes. """
         self._errors[name]=True
         self._failed = True
-    # fail cleanup before every item
     def _clean(self)->None:
+        """ fail cleanup before every item """
         self._failed = False
         self._errors = {
             'title_header' : False,
@@ -18,45 +26,43 @@ class OfferProcessor:
             'price_holder' : False,
             'price' : False
         }
-    # Regular expectation helper function. DRY regarding.
-    def _priceRegExp(self,text:str, multi:int)->float:
-        return float(multi*int(100*float(re.findall(r'\d+\.\d+', text)[0])))/100
     def _priceHolder(self, package:webdriver)->webdriver:
+        """ price holder processor. S.O.L.I.D. regarding."""
         price_holder = package.find_elements(By.CLASS_NAME,"package-price")
-        if 1 > len(price_holder):
-            self.fail('price_holder')
+        if len(price_holder) < 1:
+            self._fail('price_holder')
         return price_holder[0]
     def _title(self, element : webdriver)->str:
         header = element.find_elements(By.CLASS_NAME,"header")
-        if 1 > len(header):
+        if len(header) < 1:
             self._fail('title_header')
             return ''
         tag = header[0].find_elements(By.TAG_NAME,"h3")
-        if 1 > len(tag):
+        if len(tag) < 1:
             self._fail('title_tag')
             return ''
         return tag[0].text
     # annual price
     def _price(self, element : webdriver)->float:
         price_element = element.find_elements(By.CLASS_NAME,"price-big")
-        if 1 > len(price_element):
+        if len(price_element) < 1:
             self._fail('price')
             return 0
         if "per year" in element.text.lower():
-            return self._priceRegExp(price_element[0].text, 1)
+            return _priceRegExp(price_element[0].text, 1)
         if "per month" in element.text.lower():
-            return self._priceRegExp(price_element[0].text, 12)
+            return _priceRegExp(price_element[0].text, 12)
         self._fail('price')
         return 0
     # annual discount
     def _discount(self, element : webdriver):
         discount_element = element.find_elements(By.TAG_NAME,"p")
-        if 1 > len(discount_element):
+        if len(discount_element) < 1:
             return 0
         if "monthly price" in discount_element[0].text.lower():
-            return self._priceRegExp(discount_element[0].text, 12)
+            return _priceRegExp(discount_element[0].text, 12)
         if "yearly price" in discount_element[0].text.lower():
-            return self._priceRegExp(discount_element[0].text, 1)
+            return _priceRegExp(discount_element[0].text, 1)
         self._fail("discount")
         return 0
     def _package(self, package:webdriver)->dict:
@@ -65,14 +71,14 @@ class OfferProcessor:
         price_holder = self._priceHolder(package)
         out['title'] = self._title(package)
         if self._errors['price_holder'] is False:
-           out['price'] = self._price(price_holder)
-           out['discount'] = self._discount(price_holder)
+            out['price'] = self._price(price_holder)
+            out['discount'] = self._discount(price_holder)
         if self._failed:
-           out['failed'] = True
-           out['debug'] = copy.deepcopy(self._errors)
-
+            out['failed'] = True
+            out['debug'] = copy.deepcopy(self._errors)
         return out
     def read(self, driver:webdriver)->list:
+        """ element reader """
         out = []
         bounds = []
         order = []
@@ -88,5 +94,3 @@ class OfferProcessor:
         self._failed = False
         self._errors = {}
         self._clean()
-
-
