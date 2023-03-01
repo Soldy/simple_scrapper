400b714b49a6dbdc74b2406d55c7a78aab458c6b
Soldy <4786022+Soldy@users.noreply.github.com>
2023-03-01 22:00:39 +0000

    classco
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ libs/classco.py
@@ -0,0 +1,27 @@
+"""
+" Class model serializer
+"""
+
+
+class ClassSerialClass:
+    def add(self, elem:str)->None:
+        self.collection_num[elem] = self.collection_num[elem] + 1
+    def lesrn(self)->None:
+         for i in sef.collection_num:
+             learnElem(i)
+    def learnElem(self, elem:str)->None:
+        self.collection_size[elem] = []
+        for i in range(sef.collection_num[elem]):
+            learnElenOne(elem, i)
+    def learnElemOne(self, elem:str, i:int)->None:
+
+    def detected(self, attr:str)->None:
+        for i in attr.split(' '):
+            self.add(i)
+            self.
+
+    def __init__(self):
+        self.collection_num = {}
+        self.collection_size = {}
+
+
