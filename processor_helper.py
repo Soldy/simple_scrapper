006045ffe01ca5a5668781a9cc9379db86c1b8dd
Soldy <4786022+Soldy@users.noreply.github.com>
2023-06-06 23:44:57 +0100

    screenshot helper
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- processor_helper.py
+++ processor_helper.py
@@ -2,6 +2,7 @@
 " Processor helper
 """
 import re
+import time
 from selenium import webdriver
 
 def getAttributes(tag:webdriver)->dict:
@@ -14,3 +15,33 @@ def getAttributes(tag:webdriver)->dict:
     for attr in attributes:
         out[attr] = tag.get_attribute(attr)
     return out
+
+
+def scrollDown(driver:webdriver)->int:
+    last = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.5)
+        height = driver.execute_script("return document.body.scrollHeight")
+        if height == last:
+            break
+        last = height
+
+def minHeight(driver:webdriver)->int:
+    size = driver.execute_script("return document.body.offsetHeight")
+
+    if 1080 > size:
+        size = 1080
+    return size
+
+def fullShot(driver:webdriver)->dict:
+    driver.set_window_size(1920, 1080)
+    driver.set_window_size(
+       1920,
+       minHeight(driver)
+    )
+    try:
+        driver.find_element_by_tag_name('body').screenshot("image.png");
+    except:
+        driver.save_screenshot("image.png");
+
