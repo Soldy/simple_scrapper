34bf86c7858dc4c43871475f40a3b600f1bff3ab
Soldy <4786022+Soldy@users.noreply.github.com>
2023-06-09 23:45:28 +0100

    cleanup
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 """
-Simple scrapper main
+" Simple scrapper main
 """
 import sys
 import json
 import scrapper
 
+
+log = []
 with scrapper.ScrapperLayer() as scrap:
     file = open('out.json', 'w')
-    file.write(json.dumps(scrap.pageGet(sys.argv[1])))
+    result = {}
+    result['url'] = sys.argv[1]
+    result['out'] = scrap.pageGet(sys.argv[1])
+    log.append(result)
+    file.write(json.dumps(log))
     file.close()
--- processor.py
+++ processor.py
@@ -2,23 +2,33 @@
 " processor class
 """
 from selenium import webdriver
+from processor_helper import fullShot
+from processor_helper import scrollDown
+from processor_harmony import HarmonyProcessor
 from processor_meta import MetaProcessor
 from processor_link import LinkProcessor
 from processor_title import TitleProcessor
 from processor_table import TableProcessor
+from processor_form import FormProcessor
 
 class Processor:
     def read(self, driver:webdriver)->dict:
+        scrollDown(driver);
+        fullShot(driver);
         out = {}
+        out['harmony'] = self._harmony.read(driver)
         out['url'] = driver.current_url
         out['title'] = self._title.read(driver)
         out['meta'] = self._meta.read(driver)
+        out['form'] = self._form.read(driver)
         out['links'] = self._link.read(driver)
         out['tables'] = self._table.read(driver)
         print(out)
         return out
     def __init__(self):
+        self._harmony  = HarmonyProcessor()
         self._title = TitleProcessor()
         self._meta  = MetaProcessor()
+        self._form = FormProcessor()
         self._link  = LinkProcessor()
         self._table = TableProcessor()
